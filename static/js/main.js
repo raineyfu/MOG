a6228792430afae097b0ce3b745cb4a9c336b84f
tongdav3 <102389546+tongdav3@users.noreply.github.com>
2022-06-18 14:30:40 -0700

    update calendar

--- static/js/main.js
+++ static/js/main.js
@@ -24,3 +24,28 @@ var calendar = app.calendar.create({
     }
   }
 });
+
+var list = document.getElementsByClassName("calendar-day");
+for (var counter = 0; counter < list.length; counter++) {
+  list[counter].addEventListener("click", function() {;
+    let text;
+    let redirect = prompt("Which journal entry would you like to view?", "Entry 1");
+    
+    switch(redirect) {
+      case "Entry 1":
+        window.location.replace("journal.html");
+        document.getElementById("demo2").innerHTML = result;
+        break;
+      case "Entry 2":
+        window.location.replace("journal.html");
+        document.getElementById("demo2").innerHTML = result;
+        break;
+      case "Entry 3":
+        window.location.replace("journal.html");
+        document.getElementById("demo2").innerHTML = result;
+        break;
+      default:
+        text = "No journal entry found!";
+    }  
+  });
+}
