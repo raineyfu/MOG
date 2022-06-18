42584b6283f4ffaa84b6dcaf070890a171ea2730
Rainey <59759137+raineyfu@users.noreply.github.com>
2022-06-18 10:39:43 -0700

    side bar changes

--- scripts.js
+++ scripts.js
@@ -4,16 +4,24 @@ var taskListOpen = false;
 document.getElementById("taskList").addEventListener("click", function(event) {
     taskListOpen = !taskListOpen;
     var taskList = document.getElementById("taskList");
-
+    
+    var taskListParent = taskList.parentElement;
     if (taskListOpen) {
-        for (var counter = 0; counter < taskList.children.length; counter++) {
-            var child = taskList.children[counter];
-            child.style.display = "block";
+        for (var counter = 0; counter < taskListParent.children.length; counter++) {
+            var child = taskListParent.children[counter];
+            console.log(child);
+            if (child.classList.contains("subItem") && taskList != child) {
+                child.classList.add("slide-bottom");
+                child.style.display = "block";
+            }
         }
     } else {
-        for (var counter = 0; counter < taskList.children.length; counter++) {
-            var child = taskList.children[counter];
-            child.style.display = "none";
+        for (var counter = 0; counter < taskListParent.children.length; counter++) {
+            var child = taskListParent.children[counter];
+            if (child.classList.contains("subItem")) {
+                child.classList.remove("slide-bottom");
+                child.style.display = "none";
+            }
         }
     }
 });
